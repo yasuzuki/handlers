e109e141fd881b42b659220cd916154edbd248ee
Yasunori Suzuki <railsyas@gmail.com>
2013-02-20 22:31:19 +0900

    [Differences] + Created handler plugin.
    [Reasons]
    + To work template handler.

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '0cbac52dde8ab9ea3801d3c5d82eb7eebe058cd71e281c7ad6966b918d38e25b633236a4a328ee3776b2d3b937d98607d7f6c76546c999e5d3e80238fa8e780d'
